34bc3c80160ff5486a691b022c51f01e446bd9a4
IS1-4MJ <30614712+IS1-4MJ@users.noreply.github.com>
2021-07-10 12:23:07 -0400

    Built genericmodel v1
    v1

--- Calendar-Auto-Scheduler/calendarauto/models.py
+++ Calendar-Auto-Scheduler/calendarauto/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class GenericTask(models.Model):
+    
+    task_name = models.CharField(max_length=100)
+    task_description = models.CharField(max_length=300)
+    deadline = models.DateTimeField('Deadline')
+    do_after = models.DateTimeField('Do After', default=timezone.now()) # Do the task by 
+    est_time_to_complete = models.TimeField("est. time to complete") # Est time to complete
+    completed = models.BooleanField(default=False) # Whether the task is completed or not
+    location = models.CharField(max_length = 300) # The location of the task
+    scheduled = models.BooleanField(default=False) # Whether the task is scheduled or not
+
+    time_created = models.DateTimeField("Time Created", default=timezone.now())
+
+    def get_starting_deadline(self):
+        return self.deadline - self.est_time_to_complete
+    
+class GenericActiveTask(GenericTask):
+    pass
+class GenericInactiveTask(GenericTask):
+    pass
+
+class GenericHourBlock(models.Model):
+    hour_type = models.CharField(max_length=2, default='BL') # The type of the hour
+    datetime = models.DateTimeField('Hour Representative')
+    isFilled = models.BooleanField(default=False)
+    current_task = models.ForeignKey(GenericTask, ondelete=models.CASCADE)
+    
+    def populate(self):
+        options = GenericActiveTask.objects.all()
+        selection = [x for ]
+        
