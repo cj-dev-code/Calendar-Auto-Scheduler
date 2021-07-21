f8190748de5b4c13af453766a81043b072b4b103
IS1-4MJ <30614712+IS1-4MJ@users.noreply.github.com>
2021-07-21 11:53:00 -0400

    Made some view handlers for the add new task function
    w

--- Calendar-Auto-Scheduler/calendarauto/views.py
+++ Calendar-Auto-Scheduler/calendarauto/views.py
@@ -6,7 +6,10 @@ from django.template import loader
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from datetime import datetime, time
 import datetime
+
+from .models import GenericTask, GenericHourBlock
 
 
 #from calendarauto.models import Article
@@ -14,19 +17,9 @@ import datetime
 # If the url is going to use varibles, then they must be listed here
 # in the function name if it is a custom view i.e. what we have here
 def CalendarView(request, year, month, day):
-    #template_name = 'calendarauto/calendar.html'
-    #queryset = Article.objects.all()
-    #date_field = "pub_date"
-    #week_format = "%W"
-    #allow_future = True
-    #try:
-    #    article = Article.objects.get(pk=question_id)
-    #except Article.DoesNotExist:
-    #    raise Http404("Article does not exist")
-    #if (datetime.datetime(year = year, month, day)):
     isValidDate = True
     try:
-        datetime.datetime(year, month, day)
+        datetime(year, month, day)
     except:
         isValidDate = False
             
@@ -42,8 +35,6 @@ def CalendarView(request, year, month, day):
     sat = today + datetime.timedelta(6 - idx)
     return render(request, 'calendarauto/calendar.html', {'sunday' : sun, 'saturday' : sat, 'day' : day})
     
-    
-    
 class TodoView(generic.ListView):
     template_name = 'calendarauto/todolist.html'
     context_object_name = 'task_list'
@@ -51,7 +42,33 @@ class TodoView(generic.ListView):
 def MysteryView(request):
     return render(request, 'calendarauto/calendar.html')
     
+def create_generic_task(deadline, hours_to_complete,
+                    completed=False,scheduled=False, time_created=None, location='Default',
+                    task_name='Default', task_description='Default',
+                    do_after = None):
+    est_time = time(int(hours_to_complete), int((hours_to_complete % 1)*60), 0)
+    time_assigned = timezone.now() if time_created == None else time_created
+    do_after = timezone.now() if do_after == None else do_after
+    return GenericTask.objects.create(est_time_to_complete=est_time,
+                                      time_created = time_assigned,
+                                      scheduled = False,
+                                      location = location,
+                                      completed = False,
+                                      do_after = do_after,
+                                      deadline = deadline,
+                                      task_description=task_description,
+                                      task_name=task_name)
+
 def add_new_task(request,year, month, day):
     print(str(request.POST))
     print("Hello world")
-    return HttpResponseRedirect(reverse('calendarauto:calendar_view',args=(year, month,day)))#'calendarauto:calendarauto', args=(year, month,day)))
+    
+    deadline_year = int(request.POST['end_time'][:4])
+    deadline_month = int(request.POST['end_time'][5:7])
+    deadline_day = int(request.POST['end_time'][8:10])
+    deadline_hr = int(request.POST['end_time'][11:13])
+    deadline_min = int(request.POST['end_time'][14:16])
+    
+    #create_generic_task()
+    return HttpResponseRedirect(reverse('calendarauto:calendar_view', args=(year, month, day)))
+    #return HttpResponseRedirect(reverse('calendarauto:calendar_view',args=(year, month,day)))#'calendarauto:calendarauto', args=(year, month,day)))
